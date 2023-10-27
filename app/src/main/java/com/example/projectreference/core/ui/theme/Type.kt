fd6aea2c49e9a093db35015cb3f590790b1ada26
Leonardo Santana <lp_santana@hotmail.com>
2023-10-27 18:10:01 -0300

    Using single urbanist family

--- app/src/main/java/com/example/projectreference/core/ui/theme/Type.kt
+++ app/src/main/java/com/example/projectreference/core/ui/theme/Type.kt
@@ -4,46 +4,27 @@ import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontListFontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.projectreference.R
 
-private val UrbanistExtraBold = FontFamily(Font(R.font.urbanist_extrabold))
-private val UrbanistBold = FontFamily(Font(R.font.urbanist_bold))
-private val UrbanistLight = FontFamily(Font(R.font.urbanist_light))
-private val UrbanistMedium = FontFamily(Font(R.font.urbanist_medium))
-private val UrbanistSemiBold = FontFamily(Font(R.font.urbanist_semi_bold))
-private val UrbanistRegular = FontFamily(Font(R.font.urbanist_regular))
+private val UrbanistExtraBold = Font(R.font.urbanist_extrabold, FontWeight.ExtraBold)
+private val UrbanistBold = Font(R.font.urbanist_bold, FontWeight.Bold)
+private val UrbanistLight = Font(R.font.urbanist_light, FontWeight.Light)
+private val UrbanistMedium = Font(R.font.urbanist_medium, FontWeight.Medium)
+private val UrbanistSemiBold = Font(R.font.urbanist_semi_bold, FontWeight.SemiBold)
+private val UrbanistRegular = Font(R.font.urbanist_regular, FontWeight.Normal)
+
+private val UrbanistFamily = FontFamily(
+    UrbanistExtraBold,
+    UrbanistBold,
+    UrbanistLight,
+    UrbanistMedium,
+    UrbanistSemiBold,
+    UrbanistRegular
+)
 
 val typography = Typography(
-    defaultFontFamily = UrbanistRegular
+    defaultFontFamily = UrbanistFamily
 )
-//val typography = Typography(
-//    h1 = TextStyle(
-//        fontFamily = UrbanistExtraBold,
-//        fontSize = 40.sp,
-//    ),
-//    h2 = TextStyle(
-//        fontFamily = UrbanistExtraBold,
-//        fontSize = 36.sp
-//    ),
-//    h3 = TextStyle(
-//        fontFamily = UrbanistSemiBold,
-//        fontSize = 13.sp
-//    ),
-//    subtitle1 = TextStyle(
-//        fontFamily = UrbanistMedium,
-//        fontSize = 15.sp
-//    ),
-//    body1 = TextStyle(
-//        fontFamily = UrbanistRegular,
-//        fontSize = 13.sp
-//    ),
-//    button = TextStyle(
-//        fontFamily = UrbanistBold,
-//        fontSize = 13.sp
-//    ),
-//    caption = TextStyle(
-//        fontFamily = UrbanistRegular,
-//        fontSize = 12.sp
-//    )
-//)
