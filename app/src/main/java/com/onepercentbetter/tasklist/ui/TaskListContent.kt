0d1faa4219e818bf075623e2a85605110ef036a2
Leonardo Santana <leonardopontes.santana@gmail.com>
2024-02-06 06:40:34 -0300

    Adding padding to fab

--- app/src/main/java/com/onepercentbetter/tasklist/ui/TaskListContent.kt
+++ app/src/main/java/com/onepercentbetter/tasklist/ui/TaskListContent.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
@@ -154,7 +155,8 @@ private fun ToolbarIconButton(
 private fun AddTaskButton(onAddButtonClicked: () -> Unit) {
     FloatingActionButton(
         onClick = onAddButtonClicked,
-        shape = CircleShape
+        shape = CircleShape,
+        modifier = Modifier.navigationBarsPadding()
     ) {
         Icon(
             Icons.Default.Add,
