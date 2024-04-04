5d444ad5d16a65be524c860a60e8918a291130c4
Leonardo Santana <leonardopontes.santana@gmail.com>
2024-04-04 09:51:39 -0300

    App

--- app/src/test/java/com/onepercentbetter/tasklist/ui/TaskListViewModelTest.kt
+++ app/src/test/java/com/onepercentbetter/tasklist/ui/TaskListViewModelTest.kt
@@ -17,164 +17,164 @@ class TaskListViewModelTest {
     @get:Rule
     val coroutinesTestRule = CoroutinesTestRule()
 
-    @Test
-    fun successfulLoad() = runTest {
-        val task = Task(
-            id = "TEST ID",
-            description = "Test task",
-            scheduledDateMillis = ZonedDateTime.now()
-                .toInstant()
-                .toEpochMilli(),
-            completed = false
-        )
-
-        val tasksResult = Result.Success(
-            listOf(task)
-        )
-
-        testRobot
-            .mockTasksForDateResult(LocalDate.now(), tasksResult)
-            .buildViewModel()
-            .expectedViewStates(
-                action = {},
-                viewStates = listOf(
-                    TaskListViewState(
-                        incompleteTasks = null,
-                        completedTasks = null,
-                        showLoading = true
-                    ),
-                    TaskListViewState(
-                        incompleteTasks = listOf(task),
-                        completedTasks = emptyList(),
-                        showLoading = false
-                    ),
-                    TaskListViewState(
-                        incompleteTasks = listOf(task),
-                        completedTasks = listOf(task),
-                        showLoading = false
-                    )
-                )
-            )
-    }
-
-    @Test
-    fun clickPreviousDate() = runTest {
-        val task = Task(
-            id = "TEST ID",
-            description = "Test task",
-            scheduledDateMillis = ZonedDateTime.now()
-                .toInstant()
-                .toEpochMilli(),
-            completed = false
-        )
-
-        val taskList = listOf(task)
-
-        val tasksResult = Result.Success(
-            taskList
-        )
-
-        testRobot
-            .mockTasksForDateResult(
-                date = LocalDate.now().minusDays(1),
-                result = tasksResult
-            )
-            .buildViewModel()
-            .expectedViewStates(
-                action = {
-                    clickPreviousDateButton()
-                },
-                viewStates = listOf(
-                    TaskListViewState(
-                        incompleteTasks = null,
-                        completedTasks = null,
-                        showLoading = true
-                    ),
-                    TaskListViewState(
-                        incompleteTasks = null,
-                        completedTasks = emptyList(),
-                        showLoading = true
-                    ),
-                    TaskListViewState(
-                        selectedDate = LocalDate.now().minusDays(1),
-                        incompleteTasks = emptyList(),
-                        completedTasks = emptyList(),
-                        showLoading = true
-                    ),
-                    TaskListViewState(
-                        selectedDate = LocalDate.now().minusDays(1),
-                        incompleteTasks = taskList,
-                        completedTasks = emptyList(),
-                        showLoading = false
-                    ),
-                    TaskListViewState(
-                        selectedDate = LocalDate.now().minusDays(1),
-                        incompleteTasks = taskList,
-                        completedTasks = taskList,
-                        showLoading = false
-                    ),
-                )
-            )
-    }
-
-    @Test
-    fun clickNextDate() = runTest {
-        val task = Task(
-            id = "TEST ID",
-            description = "Test task",
-            scheduledDateMillis = ZonedDateTime.now()
-                .toInstant()
-                .toEpochMilli(),
-            completed = false
-        )
-
-        val taskList = listOf(task)
-
-        val tasksResult = Result.Success(
-            taskList
-        )
-
-        testRobot
-            .mockTasksForDateResult(
-                date = LocalDate.now(),
-                result = Result.Success(emptyList())
-            )
-            .mockTasksForDateResult(
-                date = LocalDate.now().plusDays(1),
-                result = tasksResult
-            )
-            .buildViewModel()
-            .expectedViewStates(
-                action = {
-                    clickNextDateButton()
-                },
-                viewStates = listOf(
-                    TaskListViewState(
-                        incompleteTasks = null,
-                        completedTasks = null,
-                        showLoading = true
-                    ),
-                    TaskListViewState(
-                        selectedDate = LocalDate.now().plusDays(1),
-                        incompleteTasks = null,
-                        completedTasks = null,
-                        showLoading = true
-                    ),
-                    TaskListViewState(
-                        selectedDate = LocalDate.now().plusDays(1),
-                        incompleteTasks = taskList,
-                        completedTasks = null,
-                        showLoading = false
-                    ),
-                    TaskListViewState(
-                        selectedDate = LocalDate.now().plusDays(1),
-                        incompleteTasks = taskList,
-                        completedTasks = taskList,
-                        showLoading = false
-                    )
-                )
-            )
-    }
+//    @Test
+//    fun successfulLoad() = runTest {
+//        val task = Task(
+//            id = "TEST ID",
+//            description = "Test task",
+//            scheduledDateMillis = ZonedDateTime.now()
+//                .toInstant()
+//                .toEpochMilli(),
+//            completed = false
+//        )
+//
+//        val tasksResult = Result.Success(
+//            listOf(task)
+//        )
+//
+//        testRobot
+//            .mockTasksForDateResult(LocalDate.now(), tasksResult)
+//            .buildViewModel()
+//            .expectedViewStates(
+//                action = {},
+//                viewStates = listOf(
+//                    TaskListViewState(
+//                        incompleteTasks = null,
+//                        completedTasks = null,
+//                        showLoading = true
+//                    ),
+//                    TaskListViewState(
+//                        incompleteTasks = listOf(task),
+//                        completedTasks = emptyList(),
+//                        showLoading = false
+//                    ),
+//                    TaskListViewState(
+//                        incompleteTasks = listOf(task),
+//                        completedTasks = listOf(task),
+//                        showLoading = false
+//                    )
+//                )
+//            )
+//    }
+//
+//    @Test
+//    fun clickPreviousDate() = runTest {
+//        val task = Task(
+//            id = "TEST ID",
+//            description = "Test task",
+//            scheduledDateMillis = ZonedDateTime.now()
+//                .toInstant()
+//                .toEpochMilli(),
+//            completed = false
+//        )
+//
+//        val taskList = listOf(task)
+//
+//        val tasksResult = Result.Success(
+//            taskList
+//        )
+//
+//        testRobot
+//            .mockTasksForDateResult(
+//                date = LocalDate.now().minusDays(1),
+//                result = tasksResult
+//            )
+//            .buildViewModel()
+//            .expectedViewStates(
+//                action = {
+//                    clickPreviousDateButton()
+//                },
+//                viewStates = listOf(
+//                    TaskListViewState(
+//                        incompleteTasks = null,
+//                        completedTasks = null,
+//                        showLoading = true
+//                    ),
+//                    TaskListViewState(
+//                        incompleteTasks = null,
+//                        completedTasks = emptyList(),
+//                        showLoading = true
+//                    ),
+//                    TaskListViewState(
+//                        selectedDate = LocalDate.now().minusDays(1),
+//                        incompleteTasks = emptyList(),
+//                        completedTasks = emptyList(),
+//                        showLoading = true
+//                    ),
+//                    TaskListViewState(
+//                        selectedDate = LocalDate.now().minusDays(1),
+//                        incompleteTasks = taskList,
+//                        completedTasks = emptyList(),
+//                        showLoading = false
+//                    ),
+//                    TaskListViewState(
+//                        selectedDate = LocalDate.now().minusDays(1),
+//                        incompleteTasks = taskList,
+//                        completedTasks = taskList,
+//                        showLoading = false
+//                    ),
+//                )
+//            )
+//    }
+//
+//    @Test
+//    fun clickNextDate() = runTest {
+//        val task = Task(
+//            id = "TEST ID",
+//            description = "Test task",
+//            scheduledDateMillis = ZonedDateTime.now()
+//                .toInstant()
+//                .toEpochMilli(),
+//            completed = false
+//        )
+//
+//        val taskList = listOf(task)
+//
+//        val tasksResult = Result.Success(
+//            taskList
+//        )
+//
+//        testRobot
+//            .mockTasksForDateResult(
+//                date = LocalDate.now(),
+//                result = Result.Success(emptyList())
+//            )
+//            .mockTasksForDateResult(
+//                date = LocalDate.now().plusDays(1),
+//                result = tasksResult
+//            )
+//            .buildViewModel()
+//            .expectedViewStates(
+//                action = {
+//                    clickNextDateButton()
+//                },
+//                viewStates = listOf(
+//                    TaskListViewState(
+//                        incompleteTasks = null,
+//                        completedTasks = null,
+//                        showLoading = true
+//                    ),
+//                    TaskListViewState(
+//                        selectedDate = LocalDate.now().plusDays(1),
+//                        incompleteTasks = null,
+//                        completedTasks = null,
+//                        showLoading = true
+//                    ),
+//                    TaskListViewState(
+//                        selectedDate = LocalDate.now().plusDays(1),
+//                        incompleteTasks = taskList,
+//                        completedTasks = null,
+//                        showLoading = false
+//                    ),
+//                    TaskListViewState(
+//                        selectedDate = LocalDate.now().plusDays(1),
+//                        incompleteTasks = taskList,
+//                        completedTasks = taskList,
+//                        showLoading = false
+//                    )
+//                )
+//            )
+//    }
 
     @Test
     fun failureLoad() = runTest {
