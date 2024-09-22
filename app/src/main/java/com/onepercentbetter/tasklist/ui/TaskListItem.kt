821bfc7064140143a5ce6698696952f995407ee6
Leonardo <leonardopontes.santana@gmail.com>
2024-09-22 11:41:54 -0300

    App

--- app/src/main/java/com/onepercentbetter/tasklist/ui/TaskListItem.kt
+++ app/src/main/java/com/onepercentbetter/tasklist/ui/TaskListItem.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -33,7 +34,10 @@ fun TaskListItem(
     onDoneClicked: () -> Unit,
     modifier: Modifier = Modifier,
 ) {
-    Card(modifier = modifier) {
+    Card(
+        backgroundColor = MaterialTheme.colorScheme.secondaryContainer,
+        modifier = modifier
+    ) {
         Column(
             modifier =
                 Modifier
