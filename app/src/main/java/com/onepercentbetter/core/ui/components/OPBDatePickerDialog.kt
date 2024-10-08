48848911d5df224cb6f186237ef4a58a8e64ec41
Leonardo <leonardopontes.santana@gmail.com>
2024-10-08 16:23:34 -0300

    App

--- app/src/main/java/com/onepercentbetter/core/ui/components/OPBDatePickerDialog.kt
+++ app/src/main/java/com/onepercentbetter/core/ui/components/OPBDatePickerDialog.kt
@@ -2,26 +2,19 @@ package com.onepercentbetter.core.ui.components
 
 import android.annotation.SuppressLint
 import android.content.res.Configuration
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DatePickerState
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import com.onepercentbetter.ExcludeFromJacocoGeneratedReport
 import com.onepercentbetter.core.ui.theme.OPBTheme
+import java.time.LocalDate
 
 /**
  * This is a [Dialog] that will render a [DatePicker] and allow the user to select any date today
@@ -35,54 +28,28 @@ fun OPBDatePickerDialog(
     onDismiss: () -> Unit,
     onComplete: (Long?) -> Unit,
 ) {
-    Dialog(
+    DatePickerDialog(
         onDismissRequest = onDismiss,
-    ) {
-        Surface {
-            Column {
-                DatePicker(
-                    datePickerState,
-//                    dateValidator = { dateMillis ->
-//                        val todayStartMillis = LocalDate.now().toEpochMillisUTC()
-//
-//                        dateMillis >= todayStartMillis
-//                    },
-                )
-
-                DatePickerButtonRow(
-                    onCancelClicked = onDismiss,
-                    onDoneClicked = {
-                        onComplete(datePickerState.selectedDateMillis)
-                    },
+        confirmButton = {
+            TextButton(
+                onClick = {
+                    onComplete.invoke(datePickerState.selectedDateMillis)
+                },
+            ) {
+                Text(
+                    text = "DONE",
                 )
             }
-        }
-    }
-}
-
-@Composable
-private fun DatePickerButtonRow(
-    onCancelClicked: () -> Unit,
-    onDoneClicked: () -> Unit,
-) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalArrangement = Arrangement.End,
+        },
     ) {
-        TextButton(
-            onClick = onCancelClicked,
-        ) {
-            Text("CANCEL")
-        }
-
-        Spacer(modifier = Modifier.width(8.dp))
-
-        TextButton(
-            onClick = onDoneClicked,
-        ) {
-            Text("DONE")
-        }
+        DatePicker(
+            datePickerState,
+//            dateValidator = { dateMillis ->
+//                val todayStartMillis = LocalDate.now().toEpochMillisUTC()
+//
+//                dateMillis >= todayStartMillis
+//            },
+        )
     }
 }
 
