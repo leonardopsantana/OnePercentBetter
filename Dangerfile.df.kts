bb467f600169de4c145fa241da5f70b11a2a6b4b
Leonardo <leonardopontes.santana@gmail.com>
2024-11-13 11:56:42 -0300

    Add detekt reports to PRs

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,11 +1,14 @@
+@file:DependsOn("xyz.pavelkorolev.danger.detekt:plugin:1.2.0")
 @file:Suppress("MagicNumber", "WildcardImport", "ForbiddenComment")
 
 // Editing this file: https://github.com/danger/kotlin?tab=readme-ov-file#autocomplete-and-syntax-highlighting-in-intellij-idea-or-android-studio
 import systems.danger.kotlin.*
 import java.io.File
+import systems.danger.kotlin.models.github.*
+import xyz.pavelkorolev.danger.detekt.DetektPlugin
 
 danger(args) {
-
+    warnDetekt()
     onGitHub {
         val additions = pullRequest.additions ?: 0
         val deletions = pullRequest.deletions ?: 0
@@ -33,5 +36,28 @@ danger(args) {
                 ":construction: PR is marked with Work in Progress (WIP)",
             )
         }
+    }
+}
+
+fun warnDetekt() {
+    val file = File("build/reports/detekt/report.sarif")
+    if (!file.exists()) {
+        warn(
+            "🙈 No detekt report found",
+        )
+        return
+    }
+    with(DetektPlugin) {
+        val report = parse(file)
+        val count = report.count
+        if (count == 0) {
+            message("👏👏👏 Good job! Detekt found no violations here!")
+            return
+        }
+        fail(
+            "🙁 Detekt violations found: **${report.count}**.\n" +
+                "Please fix them to proceed. We have zero-warning policy"
+        )
+        report(report)
     }
 }
